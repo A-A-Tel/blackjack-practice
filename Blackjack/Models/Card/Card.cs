9afb5ce98d46cbf452b77ed899abdad38aadbded
A-A-Tel <anouaranthonytel@gmail.com>
2026-03-18 10:50:58 +0100

    added card class

---
+++ Blackjack/Models/Card/Card.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace Blackjack.Models.Card;
+
+public class Card
+{
+    private static readonly Dictionary<CardRank, int> BlackjackValues = new()
+    {
+        { CardRank.Two, 2 },
+        { CardRank.Three, 3 },
+        { CardRank.Four, 4 },
+        { CardRank.Five, 5 },
+        { CardRank.Six, 6 },
+        { CardRank.Seven, 7 },
+        { CardRank.Eight, 8 },
+        { CardRank.Nine, 9 },
+        { CardRank.Ten, 10 },
+        { CardRank.Jack, 10 },
+        { CardRank.Queen, 10 },
+        { CardRank.King, 10 },
+        { CardRank.Ace, 11 }
+    };
+
+    public Card(CardRank rank, CardSuit suit, bool flipped)
+    {
+        Rank = rank;
+        Suit = suit;
+        Flipped = flipped;
+        Value = BlackjackValues[rank];
+    }
+
+    public CardRank Rank { get; init; }
+    public CardSuit Suit { get; init; }
+    public int Value { get; init; }
+    public bool Flipped { get; private set; }
+
+    public void Flip()
+    {
+        Flipped = !Flipped;
+    }
+}
---
+++ Blackjack/Models/Card/CardRank.cs
@@ -0,0 +1,18 @@
+namespace Blackjack.Models.Card;
+
+public enum CardRank
+{
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
