e90f0439cd4eb9a36e2f04f25110ef56d8a89912
A-A-Tel <anouaranthonytel@gmail.com>
2026-03-19 10:30:18 +0100

    added hand valueing

---
+++ Blackjack/Models/Card/CardHand.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Blackjack.Models.Card;
+
+public class CardHand
+{
+    private readonly List<Card> _cards = [];
+
+    public void Hit(Card card)
+    {
+        _cards.Add(card);
+    }
+
+    public int GetValue()
+    {
+        int aceCount = 0;
+        int value = 0;
+        
+        foreach (Card card in _cards)
+        {
+            if (card.Rank == CardRank.Ace) aceCount++;
+            value += card.Value;
+        }
+
+        while (value > 21 && aceCount > 0)
+        {
+            value -= 10;
+            aceCount--;
+        }
+
+        return value;
+    }
+}
